ad31e28449c939cfe31518058f75e5b5c2a3af3e
Swapnil <swapnil.vishu.7027@gmail.com>
2021-03-24 00:53:57 +0530

    added about & error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About us!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Facere accusamus enim sit iusto, veniam iure ipsum unde amet?
+        Neque culpa voluptate possimus optio. Sequi fuga quisquam modi,
+        suscipit quos dignissimos eveniet vero? Iusto itaque nam molestias
+        beatae similique, nesciunt exercitationem!
+      </p>
+    </section>
   )
 }
 
