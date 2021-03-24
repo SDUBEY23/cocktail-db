3bbcdd5b924aabf1f0ce944c6b3a25c60e8b1177
Swapnil <swapnil.vishu.7027@gmail.com>
2021-03-25 01:23:43 +0530

    added cocktail list

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -5,7 +5,7 @@ import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
   const {cocktails, loading} = useGlobalContext();
-
+  
   if(loading){
     return <Loading/>
   }
@@ -18,9 +18,16 @@ const CocktailList = () => {
   }
   
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className="section">
+      <h2 className="section-title">
+        Cocktails
+      </h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />
+        })}
+      </div>
+    </section>
   )
 }
 
